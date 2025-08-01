90ab1bd1f15968ccd8f6ae4502fb023125b7ec18
gicks04 <gichanyoon@gmail.com>
2025-08-02 01:29:08 +0900

    [chore]: 프로필 앱의 url스켈레톤 작성

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import SchoolProfileAPIView
+from .views import (
+    SchoolProfileAPIView,
+    FreeTimeAPIView,
+    InterestTagAPIView,
+    BasicInfoAPIView,
+    AddtionalInfoAPIView,
+)
 
 
 urlpatterns = [
     path('school', SchoolProfileAPIView.as_view(), name='school-profile'),
+    path('free_time', FreeTimeAPIView.as_view(), name='free-time'),
+    path('interests', InterestTagAPIView.as_view(), name='interest-tag'),
+    path('basic_info', BasicInfoAPIView.as_view(), name='nickname-mbti'),
+    path('additional_info', AddtionalInfoAPIView.as_view(), name='additional-info'),
 ]
