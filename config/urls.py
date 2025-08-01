54c94548ad59d2020c525a63cbd3595765e9d410
Lim-Yewon <yewonim805@gmail.com>
2025-08-02 03:24:49 +0900

    feat: 프로필 1단계 나이 드롭다운 선택 기능 추가 구현

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from apps.users.views import user_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', user_login, name='home'), 
     path("users/", include("apps.users.urls", namespace="users")),
     # path('profiles/', include('apps.profiles.urls')),
     # path('schedules/', include('apps.schedules.urls')),
