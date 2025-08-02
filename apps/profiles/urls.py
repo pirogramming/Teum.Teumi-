f8dfd61e6608be831d587eefb06437f83779bffc
Lim-Yewon <yewonim805@gmail.com>
2025-08-03 05:34:49 +0900

    feat: 프로필 1단계 CSS 구성

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import SchoolProfileAPIView
-
+from .views import profile_input_page       # 임시이니 이따 지우자
 
 urlpatterns = [
     path('school', SchoolProfileAPIView.as_view(), name='school-profile'),
+    path('1/', profile_input_page, name='profile_input_page'),      # 임시이니 이따 지우자
 ]
