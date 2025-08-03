09b2f522284725eb57eb83d4f97086fa369f179e
gicks04 <gichanyoon@gmail.com>
2025-08-03 22:08:30 +0900

    [feat]: 매칭 url 작성

--- apps/matches/urls.py
+++ apps/matches/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("matches/", views.MatchListCreateView.as_view(), name="match-list-create"),  # GET, POST
+    path("matches/<int:pk>/", views.MatchStatusUpdateView.as_view(), name="match-status-update"),  # PATCH
+    path("matches/mine", views.MyMatchListView.as_view(), name="my-match-list"),  # GET with status query param
+    path("matches/recommendations", views.MatchRecommendationView.as_view(), name="match-recommendations"),  # GET
 ]
