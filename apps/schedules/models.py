4f1f645c33db82532aafaf8f97440dc6ec24c77d
Jeongmin An <ajm010402@gmail.com>
2025-08-03 18:52:12 +0900

    chore: Freetime에서 BaseEntity 상속 받기

--- apps/schedules/models.py
+++ apps/schedules/models.py
@@ -8,16 +8,13 @@ class DayOfWeek(models.TextChoices):
     THURSDAY = "Thursday", "목"
     FRIDAY = "Friday", "금"
 
-class FreeTime(models.Model):
+class FreeTime(BaseEntity):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="free_times")
     day_of_week = models.CharField(max_length=10, choices=DayOfWeek.choices)
 
     # 24시간단위로 저장
     start_time = models.TimeField(null=True, blank=True)
     end_time = models.TimeField(null=True, blank=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return f"{self.user} - {self.day_of_week} {self.start_time} ~ {self.end_time}"
