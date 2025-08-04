6a31f03b859b127e2401bd38432722ebdaac5689
gicks04 <gichanyoon@gmail.com>
2025-08-04 14:30:58 +0900

    [feat]:리뷰앱 API생성

--- apps/reviews/urls.py
+++ apps/reviews/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("", views.ReviewCreateView.as_view(), name="review-create"),   # POST
+    path("", views.ReviewListView.as_view(), name="review-list"),       # GET
 ]
