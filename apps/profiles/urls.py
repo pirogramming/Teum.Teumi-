b1f0aece0c78e60eeb1c58cfb7f20df9320d5989
gicks04 <gichanyoon@gmail.com>
2025-08-04 20:48:38 +0900

    [fix]:임시 url삭제

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
     path('interests', InterestTagAPIView.as_view(), name='interest-tag'),
     path('basic_info', BasicInfoAPIView.as_view(), name='nickname-mbti'),
     path('additional_info', AddtionalInfoAPIView.as_view(), name='additional-info'),
-    path('1/', profile_input_page, name='profile_input_page'),      # 임시이니 이따 지우자
 ]
