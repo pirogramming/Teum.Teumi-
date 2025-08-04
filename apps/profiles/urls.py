718018734143b20578151d421d82dfc7eac59896
Lim-Yewon <yewonim805@gmail.com>
2025-08-04 21:04:25 +0900

    fix: 로그인창 애니메이션 구현

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -6,7 +6,6 @@ from .views import (
     BasicInfoAPIView,
     AddtionalInfoAPIView,
 )
-from .views import profile_input_page       # 임시이니 이따 지우자
 
 urlpatterns = [
     path('school', SchoolProfileAPIView.as_view(), name='school-profile'),
